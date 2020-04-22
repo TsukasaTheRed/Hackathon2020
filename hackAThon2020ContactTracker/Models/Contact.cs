dcbec0fe309980613486d8dbe3d40b98d340908a
Darren Neaverth Jr <Tsukasa589@gmail.com>
2020-04-22 15:26:27 -0400

    Making a branch in an attempt to fix an error
    Github newbie stumbling around trying to get his legs

---
+++ hackAThon2020ContactTracker/Models/Contact.cs
@@ -0,0 +1,9 @@
+namespace hackAThon2020ContactTracker.Models
+{
+    public class Contact {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
