201226e213f5430b8fb94a72bfc5b727fb8da66f
Darren Neaverth Jr <Tsukasa589@gmail.com>
2020-04-22 23:11:24 -0400

    Generated Controllers and Views with Correct DB
    Fixing a DateTime issue

--- hackAThon2020ContactTracker/Models/Contact.cs
+++ hackAThon2020ContactTracker/Models/Contact.cs
@@ -1,13 +1,14 @@
+using System;
+
 namespace hackAThon2020ContactTracker.Models
 {
     public class Contact {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
-        public string Time {get; set;}
-        public bool? showingSymptoms { get; set; }
-        public bool? livingWith { get; set; }
-        public bool? isEmployer { get; set; }
-
+        public DateTime Time { get; set;}
+        public bool showingSymptoms { get; set; }
+        public bool livingWith { get; set; }
+        public bool isEmployer { get; set; }
     }
 }
