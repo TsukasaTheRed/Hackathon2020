de556669bc085d56547bf05adcc81b90d9e57848
Darren Neaverth Jr <Tsukasa589@gmail.com>
2020-04-24 16:26:34 -0400

    Successfully Migrated changes to the Database to reflect changes in page
    Now we have Dates only

---
+++ hackAThon2020ContactTracker/Migrations/20200424202329_ChangeToDateTime.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hackAThon2020ContactTracker.Migrations
+{
+    public partial class ChangeToDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- hackAThon2020ContactTracker/Models/Contact.cs
+++ hackAThon2020ContactTracker/Models/Contact.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace hackAThon2020ContactTracker.Models
 {
@@ -6,6 +7,7 @@ namespace hackAThon2020ContactTracker.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Time { get; set;}
         public bool showingSymptoms { get; set; }
         public bool livingWith { get; set; }
