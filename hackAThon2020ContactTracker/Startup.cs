99564ea5f5d0ee36f5dd2bbb05654656cd436ffe
Darren Neaverth Jr <Tsukasa589@gmail.com>
2020-04-24 13:15:13 -0400

    Minor Housekeeping before the final 2 problems get solved
    Fixed the redirect, Deleted commented out code, and Added commented out silliness.

--- hackAThon2020ContactTracker/Startup.cs
+++ hackAThon2020ContactTracker/Startup.cs
@@ -62,7 +62,7 @@ namespace hackAThon2020ContactTracker
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Contacts}/{action=Index}/{id?}");
             });
         }
     }
